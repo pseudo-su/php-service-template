8dbdbc9f083c960a993e8d8e13bf938d2286f6d0
stableford <stableford@stablefords-MacBook-Pro.local>
2021-01-23 01:57:27 +1100

    chore: update

--- bin/http-server.php
+++ bin/http-server.php
@@ -1,20 +1,75 @@
 <?php
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
-use React\EventLoop\Factory;
-use React\Socket\Server as SocketServer;
-use Pseudo\PhpServiceTemplate\AppServer;
+use Swoole\Http\Server;
+use Swoole\Http\Request;
+use Swoole\Http\Response;
 
-$loop = Factory::create();
+use Slim\App;
 
-$appServer = new AppServer(loop: $loop);
+$host = getenv('HOST') ?: '0.0.0.0';
+$port = getenv('PORT') ?: '8080';
 
-$socketServer = new SocketServer($appServer->getUri(), $loop);
+$server = new Server($host, $port);
 
-$httpServer = $appServer->build();
-$httpServer->listen($socketServer);
+// a swoole server is evented just like express
+$server->on('start', function (Server $server) use ($host, $port) {
+    echo sprintf('Swoole http server is started at http://%s:%s' . PHP_EOL, $host, $port);
+});
 
-echo 'Listening on ' . str_replace('tcp:', 'http:', $socketServer->getAddress()) . "\n";
+// handle all requests with this response
+$server->on('request', function (Request $req, Response $res) {
+    // populate the global state with the request info
+    $_SERVER['REQUEST_URI'] = $req->server['request_uri'];
+    $_SERVER['REQUEST_METHOD'] = $req->server['request_method'];
+    $_SERVER['REMOTE_ADDR'] = $req->server['remote_addr'];
 
-$loop->run();
+    $_GET = $req->get ?? [];
+    $_POST = $req->post ?? $req->rawContent();
+    $_FILES = $req->files ?? [];
+
+    // each request should create a new App()
+    $app = new App();
+
+    // example of a JSON response
+    $app->get('/type/json', function ($request, $response, $args) {
+        return $response->withJson([
+            'status' => 'ok',
+            'message' => 'hey!'
+        ]);
+    });
+
+    // normal text/html response
+    $app->get('/[{name}]', function ($request, $response, $args) {
+        $name = $args['name'] ?? 'world!';
+
+        return $response
+            ->getBody()
+            ->write(sprintf('<p>Hello, %s</p>', $name));
+    });
+
+    // suppress output by passing "true"
+    $slim = $app->run(true);
+
+    // transfer the Slim headers to the Swoole app
+    foreach ($slim->getHeaders() as $key => $value) {
+        // content length is set when calling "end"
+        if ($key !== 'Content-Length') {
+            $res->header($key, $value[0]);
+        }
+    }
+
+    $res->status($slim->getStatusCode());
+
+    // figure out if we are running in HTTPS
+    $secure = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443);
+
+    // just add a make pretend cookie here
+    $res->cookie(explode('.', 'docker.local')[0], '1', strtotime('+1 day'), '/', getenv('HOSTNAME'), $secure , true);
+
+    // write the output
+    $res->end($slim->getBody());
+});
+
+$server->start();
--- bin/message-processor.php
+++ bin/message-processor.php
@@ -1,20 +1,78 @@
 <?php
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require_once dirname(__DIR__).'/vendor/autoload.php';
 
-use React\EventLoop\Factory;
-use React\Socket\Server as SocketServer;
-use Pseudo\PhpServiceTemplate\AppServer;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
 
-$loop = Factory::create();
+$exchange = 'events';
+$queue = 'temp_queue';
+$consumerTag = 'message-processor';
 
-$appServer = new AppServer(loop: $loop);
+$amqpHost = 'localhost';
+$amqpPort = '5672';
+$amqpUser = 'guest';
+$amqpPass = 'guest';
+$amqpVhost = '/';
 
-$socketServer = new SocketServer($appServer->getUri(), $loop);
+$connection = new AMQPStreamConnection($amqpHost, $amqpPort, $amqpUser, $amqpPass, $amqpVhost);
+$channel = $connection->channel();
 
-$httpServer = $appServer->build();
-$httpServer->listen($socketServer);
+/*
+    name: $queue
+    passive: false
+    durable: true // the queue will survive server restarts
+    exclusive: false // the queue can be accessed in other channels
+    auto_delete: false //the queue won't be deleted once the channel is closed.
+*/
+$channel->queue_declare($queue, false, true, false, false);
 
-echo 'Listening on ' . str_replace('tcp:', 'http:', $socketServer->getAddress()) . "\n";
+/*
+    name: $exchange
+    type: topic
+    passive: false
+    durable: true // the exchange will survive server restarts
+    auto_delete: false // the exchange won't be deleted once the channel is closed.
+*/
 
-$loop->run();
+$channel->exchange_declare($exchange, 'topic', false, true, false);
+
+$channel->queue_bind($queue, $exchange, routing_key: 'test');
+
+function process_message(\PhpAmqpLib\Message\AMQPMessage $message)
+{
+    echo "\n--------\n";
+    echo $message->body;
+    echo "\n--------\n";
+
+    $message->delivery_info['channel']->basic_ack($message->delivery_info['delivery_tag']);
+
+    // Send a message with the string "quit" to cancel the consumer.
+    if ($message->body === 'quit') {
+        $message->delivery_info['channel']->basic_cancel($message->delivery_info['consumer_tag']);
+    }
+}
+
+/*
+    queue: Queue from where to get the messages
+    consumer_tag: Consumer identifier
+    no_local: Don't receive messages published by this consumer.
+    no_ack: Tells the server if the consumer will acknowledge the messages.
+    exclusive: Request exclusive consumer access, meaning only this consumer can access the queue
+    nowait:
+    callback: A PHP Callback
+*/
+
+$channel->basic_consume($queue, $consumerTag, false, false, false, false, 'process_message');
+
+function shutdown(\PhpAmqpLib\Channel\AMQPChannel $channel, \PhpAmqpLib\Connection\AbstractConnection $connection)
+{
+    $channel->close();
+    $connection->close();
+}
+
+register_shutdown_function('shutdown', $channel, $connection);
+
+// Loop as long as the channel has callbacks registered
+while (count($channel->callbacks)) {
+    $channel->wait();
+}
